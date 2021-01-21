034fb01dd5d6be9decf720aae31dd7719f8d144f
Hagihara <hgrt.0822@gmail.com>
2021-01-21 14:30:49 +0900

    コメントの追加

--- Assets/Script/Title/RockButton.cs
+++ Assets/Script/Title/RockButton.cs
@@ -6,13 +6,16 @@ using UnityEngine.SceneManagement;
 
 public class RockButton : MonoBehaviour
 {
+    // プロックの耐久値の設定
     public static int GetMaterialHP()
     {
         return 100;
     }
     public void OnClickStartButton()
     {
+        //　ブロックの耐久値の代入
         Rock.HP = GetMaterialHP();
+        // PlaySceneに移動
         SceneManager.LoadScene("PlayScene");
     }
 }
