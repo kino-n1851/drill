b11b5e4f63738587a7f6e061ca50b79a99396730
Hagihara <hgrt.0822@gmail.com>
2021-01-21 14:29:45 +0900

    コメントの追加

--- Assets/Script/Title/IronButton.cs
+++ Assets/Script/Title/IronButton.cs
@@ -6,13 +6,16 @@ using UnityEngine.SceneManagement;
 
 public class IronButton : MonoBehaviour
 {
+    // プロックの耐久値の設定
     public static int GetMaterialHP()
     {
         return 200;
     }
     public void OnClickStartButton()
     {
+        //　ブロックの耐久値の代入
         Rock.HP = GetMaterialHP();
+        // PlaySceneに移動
         SceneManager.LoadScene("PlayScene");
     }
 }
